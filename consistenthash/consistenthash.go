b1a7ca6c77fac774cf47fa1d433ebbd51a838e09
jzk-code <2375679151@qq.com>
2026-06-16 19:07:05 +0800

    docs:添加一致性哈希相关注释

--- consistenthash/consistenthash.go
+++ consistenthash/consistenthash.go
@@ -26,6 +26,8 @@ func New(replicas int, fn Hash) *Map {
 	}
 	return m
 }
+
+// Add 把真实节点添加到一致性哈希环中 通过在每个真实节点前添加编号 i 来生成虚拟节点
 func (m *Map) Add(keys ...string) {
 	for _, key := range keys {
 		for i := 0; i < m.replicas; i++ {
@@ -37,6 +39,7 @@ func (m *Map) Add(keys ...string) {
 	sort.Ints(m.keys)
 }
 
+// Get 获取这个key应该在哪个真实节点上
 func (m *Map) Get(key string) string {
 	if len(m.keys) == 0 {
 		return ""
@@ -46,6 +49,6 @@ func (m *Map) Get(key string) string {
 	idx := sort.Search(len(m.keys), func(i int) bool {
 		return m.keys[i] >= hash
 	})
-
+	// 如果 idx == len(m.keys) 说明 hash 大于环上所有节点的 hash值 需要回到第一个节点
 	return m.hashMap[m.keys[idx%len(m.keys)]]
 }
